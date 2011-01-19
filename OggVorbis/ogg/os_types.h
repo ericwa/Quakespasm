eefff871a297b50f12cc8328ba7e18285cb9bce9
Eric Wasylishen <ewasylishen@gmail.com>
2011-01-19 00:15:12 -0700

    Get build working on Ubuntu

--- OggVorbis/ogg/os_types.h
+++ OggVorbis/ogg/os_types.h
@@ -24,7 +24,7 @@
 #define _ogg_realloc realloc
 #define _ogg_free    free
 
-#if defined(_WIN32) 
+#if defined(_WIN32)
 
 #  if defined(__CYGWIN__)
 #    include <stdint.h>
@@ -140,7 +140,13 @@
 
 #else
 
-#  include <ogg/config_types.h>
+#include <stdint.h>
+typedef int16_t ogg_int16_t;
+typedef uint16_t ogg_uint16_t;
+typedef int32_t ogg_int32_t;
+typedef uint32_t ogg_uint32_t;
+typedef int64_t ogg_int64_t;
+typedef uint64_t ogg_uint64_t;
 
 #endif
 
