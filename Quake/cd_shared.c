a0f74edc778e535c0ad345915147c202a47d8802
Eric Wasylishen <ewasylishen@gmail.com>
2011-01-19 01:19:09 -0700

    Update copyright notices

--- Quake/cd_shared.c
+++ Quake/cd_shared.c
@@ -2,7 +2,8 @@
 	cd_shared.c
 
 	Copyright (C) 1996-1997  Id Software, Inc.
-
+	Copyright (C) 2011 Eric Wasylishen
+ 
 	This program is free software; you can redistribute it and/or
 	modify it under the terms of the GNU General Public License
 	as published by the Free Software Foundation; either version 2
