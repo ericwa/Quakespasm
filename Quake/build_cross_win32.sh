8402a5aac0a1dea1245f41ac72f810fc32d08e97
Eric Wasylishen <ewasylishen@gmail.com>
2014-05-22 11:05:44 -0700

    update mingw build script for my pc

--- Quake/build_cross_win32.sh
+++ Quake/build_cross_win32.sh
@@ -6,20 +6,20 @@
 #TARGET=i686-w64-mingw32
 TARGET=i686-pc-mingw32
 #PREFIX=/opt/cross_win32
-PREFIX=/usr/local/cross-win32
+PREFIX=/
 
-PATH="$PREFIX/bin:$PATH"
+PATH="/c/MinGW/bin:$PATH"
 export PATH
 
 MAKE_CMD=make
 
-SDL_CONFIG=/usr/local/sdl_w32/bin/sdl-config
-CC="$TARGET-gcc"
-AS="$TARGET-as"
-RANLIB="$TARGET-ranlib"
-AR="$TARGET-ar"
-WINDRES="$TARGET-windres"
-STRIP="$TARGET-strip"
+SDL_CONFIG=/c/SDL-1.2.15/bin/sdl-config
+CC="gcc"
+AS="as"
+RANLIB="ranlib"
+AR="ar"
+WINDRES="windres"
+STRIP="strip"
 export PATH CC AS AR RANLIB WINDRES STRIP
 
 exec $MAKE_CMD SDL_CONFIG=$SDL_CONFIG CC=$CC AS=$AS RANLIB=$RANLIB AR=$AR WINDRES=$WINDRES STRIP=$STRIP -f Makefile.w32 $*
