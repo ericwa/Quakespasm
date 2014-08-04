b7b42a317c25f3005d7a2c33fd325ede8a9616ba
Eric Wasylishen <ewasylishen@gmail.com>
2014-08-04 14:49:18 -0600

    Pick default filter quality based on OS

--- Quake/snd_dma.c
+++ Quake/snd_dma.c
@@ -77,7 +77,15 @@ cvar_t		loadas8bit = {"loadas8bit", "0", CVAR_NONE};
 
 cvar_t		sndspeed = {"sndspeed", "11025", CVAR_NONE};
 cvar_t		snd_mixspeed = {"snd_mixspeed", "44100", CVAR_NONE};
-cvar_t		snd_filterquality = {"snd_filterquality", "5", CVAR_NONE};
+
+#if defined(_WIN32)
+#define SND_FILTERQUALITY_DEFAULT "5"
+#else
+#define SND_FILTERQUALITY_DEFAULT "1"
+#endif
+
+cvar_t		snd_filterquality = {"snd_filterquality", SND_FILTERQUALITY_DEFAULT,
+								 CVAR_NONE};
 
 static	cvar_t	nosound = {"nosound", "0", CVAR_NONE};
 static	cvar_t	ambient_level = {"ambient_level", "0.3", CVAR_NONE};
