2788c89acf2a7dae9eb0748f3e7725a15fc71b35
Spike <Shpoike@users.noreply.github.com>
2018-05-16 04:15:14 +0100

    Added missing header.

--- quakespasm/Quake/common.c
+++ quakespasm/Quake/common.c
@@ -28,6 +28,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #ifndef _WIN32
 #include <dirent.h>
+#include <fnmatch.h>
 #endif
 
 static char	*largv[MAX_NUM_ARGVS + 1];
